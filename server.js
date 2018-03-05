1422d378c6d12f06497be7ce348be7112d7d3e14
rajibn <36919954+rajibn@users.noreply.github.com>
2018-03-05 19:54:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -44,4 +44,9 @@ app.post('/update', function(req, res) {
 app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
 });
-
+var records = [];
+conn.query("SELECT Id,Phone, MobilePhone, FirstName, LastName, Email FROM salesforce.Contact", function(err, result) {
+  if (err) { return console.error(err); }
+  console.log("total : " + result.totalSize);
+  console.log("fetched : " + result.records.length);
+});
